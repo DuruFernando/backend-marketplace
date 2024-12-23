b92b344465898c79afde905ac3a4bf35ccc915bd
DuruFernando <fernandoduru.santos@gmail.com>
2024-12-23 11:35:41 -0300

    Create product

--- src/controllers/create-product.controller.ts
+++ src/controllers/create-product.controller.ts
@@ -1,11 +1,4 @@
-import {
-  Body,
-  Controller,
-  HttpCode,
-  Post,
-  UseGuards,
-  UsePipes,
-} from '@nestjs/common'
+import { Body, Controller, HttpCode, Post, UseGuards } from '@nestjs/common'
 import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt.auth.guard'
 import { UserPayload } from 'src/auth/jwt.strategy'
@@ -18,9 +11,9 @@ const createProductBodySchema = z.object({
   categoryId: z.string().uuid(),
   description: z.string(),
   priceInCents: z.coerce.number(),
-  status: z.string(),
 })
 
+const bodyValidationBody = new ZodValidationPipe(createProductBodySchema)
 type CreateProductBodySchema = z.infer<typeof createProductBodySchema>
 
 @Controller('/products')
@@ -30,12 +23,11 @@ export class CreateProductController {
 
   @Post()
   @HttpCode(201)
-  @UsePipes(new ZodValidationPipe(createProductBodySchema))
   async handle(
-    @Body() body: CreateProductBodySchema,
+    @Body(bodyValidationBody) body: CreateProductBodySchema,
     @CurrentUser() user: UserPayload,
   ) {
-    const { title, description, priceInCents, status, categoryId } = body
+    const { title, description, priceInCents, categoryId } = body
     const userId = user.sub
 
     await this.prisma.product.create({
@@ -43,7 +35,6 @@ export class CreateProductController {
         title,
         description,
         priceInCents,
-        status,
         ownerId: userId,
         categoryId,
       },
